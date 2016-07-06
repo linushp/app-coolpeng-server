6ea68eee8b1f87fbbcbaafb7f84df47b6cf09830
luanhaipeng <910010653@qq.com>
2016-07-06 16:04:11 +0800

    common reply function

--- cp-blog/src/main/java/com/coolpeng/app/listener/AppCacheRefreshListener.java
+++ cp-blog/src/main/java/com/coolpeng/app/listener/AppCacheRefreshListener.java
@@ -1,5 +1,8 @@
 package com.coolpeng.app.listener;
 
+import com.coolpeng.framework.db.SimpleDAO;
+import com.coolpeng.framework.db.SimpleDAOEvent;
+import com.coolpeng.framework.db.SimpleDAOEventEnum;
 import com.coolpeng.framework.event.TMSEvent;
 import com.coolpeng.framework.event.TMSEventListener;
 
@@ -8,10 +11,23 @@ import com.coolpeng.framework.event.TMSEventListener;
  */
 public class AppCacheRefreshListener extends TMSEventListener {
 
-
     @Override
     public void onEvent(TMSEvent event) {
+        if (event instanceof SimpleDAOEvent) {
+            SimpleDAOEvent daoEvent = (SimpleDAOEvent) event;
+            SimpleDAOEventEnum eventType = daoEvent.getEventType();
+            if (eventType == SimpleDAOEventEnum.afterInsert) {
+                Class entityClass = daoEvent.getClazz();
+                Object entity = daoEvent.getData();
+                //数据插入表中之后的事件
+                onEntityInsert(entityClass, entity);
+            }
+        }
+    }
 
+
+    private void onEntityInsert(Class entityClass, Object entity) {
+        //TODO 数据库中有新数据插入执行的函数
     }
 
 
