ebed14eb458c5fb89a3816b391833cd1ab6123a0
luanhaipeng <910010653@qq.com>
2016-10-17 18:16:59 +0800

    aaa

---
+++ cp-protobuf/src/main/java/com/coolpeng/proto/client/PersonClient.java
@@ -0,0 +1,14 @@
+package com.coolpeng.proto.client;
+
+import com.coolpeng.proto.pbfile.App;
+import com.google.protobuf.BlockingRpcChannel;
+import com.google.protobuf.RpcCallback;
+import com.google.protobuf.RpcController;
+import com.google.protobuf.ServiceException;
+
+/**
+ * Created by luanhaipeng on 16/10/17.
+ */
+public class PersonClient{
+
+}
